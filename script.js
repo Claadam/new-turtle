17a18263c3242009bcc690b88a14b3438f6feaf7
WalnutsAreCool <claadam@nuevaschool.org>
2016-12-07 09:21:46 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -24,14 +24,12 @@ function nSides(n) {
   }
 }
 
-function weirdSquare {
  for (i=0; i<4; i++) {
    goForward(90)
    nDegreesLeft(90)}
   for (i=0; i<12; i++) {
      goForward(90)
      nDegreesLeft(90+(i*7.5))}
-}
 
-weirdSquare()
+
 
